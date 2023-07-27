dab8d658a83c5f36f82bc075b3912e5f4ad47650
Muhammad Bangash <muhammadbangash190@gmail.com>
2023-07-27 12:19:49 +0500

    Commkit 2nd

---
+++ tranculateString.js
@@ -0,0 +1,14 @@
+function truncateString(str, num) {
+    let givenStr = str.slice("...")
+    let trunStr = ""
+  
+    for(let i = 0; i < givenStr.length; i++){
+      
+  
+    }
+  
+  
+    return str + "...";
+  }
+  
+  truncateString("A-tisket a-tasket A green and yellow basket", 8);
