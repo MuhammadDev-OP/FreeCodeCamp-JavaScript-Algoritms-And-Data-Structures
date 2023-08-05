e4cf2edc16e34c187865745b788ef5e397c7711c
Muhammad Bangash <muhammadbangash190@gmail.com>
2023-08-05 09:16:26 +0500

    commit

--- frankenSplice.js
+++ frankenSplice.js
@@ -1,5 +1,10 @@
 function frankenSplice(arr1, arr2, n) {
     let newArr = arr2.slice()
+    console.log(newArr)
+    for (let i = 0; i < arr1.length; i++) {
+    newArr.splice(n , 0, arr1[i])
+    n++
+    }
     return newArr;
   }
   
