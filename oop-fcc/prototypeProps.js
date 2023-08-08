76292f18c578707871a60ca21ebb38d37c58c2aa
Muhammad Bangash <muhammadbangash190@gmail.com>
2023-08-08 12:05:18 +0500

    Simp committed

---
+++ oop-fcc/prototypeProps.js
@@ -0,0 +1,25 @@
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
+
+let ownProps = [];
+let prototypeProps = [];
+
+// Only change code below this line
+console.log("Before");
+console.log(ownProps, prototypeProps);
+console.log("after ******** / / * *  ");
+for (let property in beagle) {
+  if (Dog.hasOwnProperty(property)) {
+    ownProps.push(property);
+  } else {
+    prototypeProps.push(property);
+  }
+}
+
+console.log(ownProps);
+console.log(prototypeProps);
